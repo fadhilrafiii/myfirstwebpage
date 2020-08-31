e24da306beaa5702a87f3fe8f7130597a7ee1cb7
fadhilrafiii <52844918+fadhilrafiii@users.noreply.github.com>
2020-08-31 17:01:29 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+location.hash = 'main';
+
+function slide() {
+  window.scrollTo(0, 720);
+}
+
+function thankyou(){
+  document.getElementById('feedback').innerHTML = "Thank you for giving feedback!"
+}
+
+function changeName(){
+  let button = document.getElementById('button-name');
+  let x = document.getElementById('your-name');
+  document.getElementById('hello').innerHTML = "Hello, " + x.value + "!";
+  button.remove();
+  x.remove();
+}
